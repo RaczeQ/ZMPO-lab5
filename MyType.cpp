fa2c733f9c13de83b0d959613bc0f2c5d6b62568
RaczeQ <raczek96@gmail.com>
2016-12-20 11:50:02 +0100

    Splitted to .h and .cpp files

--- MyType.cpp
+++ MyType.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "MyType.h"
+
+int MyType::getSum() {
+    int num = this->x;
+    int sum = 0;
+    while (num > 0) {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+int MyType::getValue() {
+    return x;
+}
+
+bool MyType::operator>(MyType m) {
+    return this->getSum() > m.getSum();
+}
+
+ostream &operator<<(ostream &os, const MyType &obj) {
+    os << obj.x;
+    return os;
+}
--- MyType.h
+++ MyType.h
@@ -16,28 +16,13 @@ private:
 public:
     MyType(int a) { x = a; }
 
-    int getSum() {
-        int num = this->x;
-        int sum = 0;
-        while (num > 0) {
-            sum = sum + num % 10;
-            num = num / 10;
-        }
-        return sum;
-    }
+    int getSum();
 
-    int getValue() {
-        return x;
-    }
+    int getValue();
 
-    bool operator > (MyType m) {
-        return this->getSum() > m.getSum();
-    }
+    bool operator > (MyType m);
 
-    friend ostream& operator<<(ostream& os, const MyType& obj) {
-        os << obj.x;
-        return os;
-    }
+    friend ostream& operator<<(ostream& os, const MyType& obj);
 
 };
 
